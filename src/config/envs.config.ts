e422771841129994a0fde0e3fe70ae89a44da99c
DANIELSSF <daniel.santiago730@gmail.com>
2024-10-28 14:16:23 -0500

    Add environment variables configuration

---
+++ src/config/envs.config.ts
@@ -0,0 +1,35 @@
+import 'dotenv/config';
+import * as joi from 'joi';
+
+import { EnvVars } from '../common/interfaces';
+
+const envsSchema = joi
+  .object({
+    PORT: joi.number().default(3000).required(),
+
+    DB_PASSWORD: joi.string().required(),
+    DB_NAME: joi.string().required(),
+    DB_HOST: joi.string().required(),
+    DB_PORT: joi.number().required(),
+    DB_USERNAME: joi.string().required(),
+  })
+  .unknown(true);
+
+const { error, value } = envsSchema.validate(process.env);
+
+if (error) throw new Error(`Config validation error: ${error.message}`);
+
+const envVars: EnvVars = value;
+
+console.log(envVars);
+
+export const envs = {
+  port: envVars.PORT,
+  db: {
+    password: envVars.DB_PASSWORD,
+    name: envVars.DB_NAME,
+    host: envVars.DB_HOST,
+    port: envVars.DB_PORT,
+    username: envVars.DB_USERNAME,
+  },
+};
