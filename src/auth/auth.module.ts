c831bff593115120b2dff6938d754bdf161636c7
CamiloCortesM <camilo.cortes1753@gmail.com>
2024-11-06 08:10:40 -0500

    Implement Google OAuth authentication flow with user management

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { authEnvs } from './config';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { User } from './entities/user.entity';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: authEnvs.jwtSecret,
+      signOptions: { expiresIn: authEnvs.jwtExpiresIn },
+    }),
+    TypeOrmModule.forFeature([
+      User,
+    ]),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
