e66a3884710f7ec6a2dbae1a3352b0a2c4bea744
CamiloCortesM <camilo.cortes1753@gmail.com>
2024-11-06 08:10:27 -0500

    Add Google OAuth environment variables and update AuthEnvVars interface

--- src/auth/config/authEnv.config.ts
+++ src/auth/config/authEnv.config.ts
@@ -7,6 +7,12 @@ const envsSchema = joi
   .object({
     JWT_SECRET: joi.string().required(),
     JWT_EXPIRES_IN: joi.string().required(),
+
+    GOOGLE_CLIENT_ID: joi.string().required(),
+    GOOGLE_CLIENT_SECRET: joi.string().required(),
+    GOOGLE_URL_CALLBACK: joi.string().required(),
+
+    FRONTEND_URL: joi.string().required(),
   })
   .unknown(true);
 
@@ -19,4 +25,12 @@ const envVars: AuthEnvVars = value;
 export const authEnvs = {
   jwtSecret: envVars.JWT_SECRET,
   jwtExpiresIn: envVars.JWT_EXPIRES_IN,
+
+  oauth: {
+   clientId: envVars.GOOGLE_CLIENT_ID,
+   clientSecret: envVars.GOOGLE_CLIENT_SECRET,
+   urlCallback: envVars.GOOGLE_URL_CALLBACK,
+  },
+  
+  frontEnd: envVars.FRONTEND_URL,
 };
--- src/auth/interfaces/authEnv.interface.ts
+++ src/auth/interfaces/authEnv.interface.ts
@@ -1,4 +1,10 @@
 export interface AuthEnvVars {
   JWT_SECRET: string;
   JWT_EXPIRES_IN: string;
+
+  GOOGLE_CLIENT_ID: string;
+  GOOGLE_CLIENT_SECRET: string;
+  GOOGLE_URL_CALLBACK: string;
+
+  FRONTEND_URL: string;
 }
