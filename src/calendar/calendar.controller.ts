9f1c1278260170f6d7826cf375c41ee5fcc02e56
CamiloCortesM <camilo.cortes1753@gmail.com>
2024-11-07 23:01:12 -0500

    Add JWT authentication to freebusy endpoint and enhance user data handling

--- src/calendar/calendar.controller.ts
+++ src/calendar/calendar.controller.ts
@@ -1,17 +1,29 @@
-import { Controller, Post, Body, HttpStatus, HttpCode } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  HttpStatus,
+  HttpCode,
+  UseGuards,
+} from '@nestjs/common';
 import { CalendarService } from './calendar.service';
 import { FreeBusyCalendarValidationDto } from './dto/search-freebusy-calendar.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { GetUser } from 'src/auth/decorators/get-user.decorator';
+import { User } from 'src/auth/entities/user.entity';
 
 @Controller('calendar')
 export class CalendarController {
   constructor(private readonly calendarService: CalendarService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post('freebusy')
   @HttpCode(HttpStatus.OK)
   generateFreeBusySchedule(
     @Body() freeBusyCalendarValidationDto: FreeBusyCalendarValidationDto,
-    //  @GetUser() user: User
+    @GetUser() user: User,
   ) {
-    return this.calendarService.getFreeBusy(freeBusyCalendarValidationDto);
+    const { emailTarget } = freeBusyCalendarValidationDto;
+    return this.calendarService.getFreeBusy({ emailTarget, user });
   }
 }
