c11612a7a6bb110e0e546b3fc52575b7ca7a8a90
CamiloCortesM <camilo.cortes1753@gmail.com>
2024-11-07 22:21:46 -0500

    Add JwtPayload interface and JwtStrategy for JWT authentication

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,31 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { User } from '../entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { authEnvs } from '../config';
+import { JwtPayload } from '../interfaces/jwt-payload.interfaces';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {
+    super({
+      secretOrKey: authEnvs.jwtSecret,
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+
+  async validate(payload: JwtPayload): Promise<User> {
+    const { id } = payload;
+
+    const user = await this.userRepository.findOneBy({ id });
+
+    if (!user) throw new UnauthorizedException('Invalid token');
+
+    return user;
+  }
+}
