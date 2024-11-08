bb7e0153b8682929ec9da63271158dad27d262d7
CamiloCortesM <camilo.cortes1753@gmail.com>
2024-11-07 22:22:46 -0500

    Integrate PaymentService into WebhookController for transaction status handling

--- src/webhook/webhook.controller.ts
+++ src/webhook/webhook.controller.ts
@@ -1,16 +1,48 @@
 // backend/src/webhook/webhook.controller.ts
 import { Controller, Post, Body } from '@nestjs/common';
+import { Transaction } from './dto/data-pay.dto';
+import { PaymentService } from 'src/payments/payment.service';
 
 @Controller('webhook')
 export class WebhookController {
+  constructor(private readonly paymentService: PaymentService) {}
   @Post()
   handleWebhook(@Body() data: any) {
-    const transaction = data.transaction;
-    console.log(data);
+    const transaction: Transaction = data.data.transaction;
 
-    // if (transaction.status === 'APPROVED') {
-    //   console.log(`Pago aprobado para referencia: ${transaction.reference}`);
-    //   // Lógica para desbloquear el recurso
-    // }
+    // APPROVED: accepted transaction.
+    // DECLINED: declined transaction.
+    // VOIDED: anuled transaction (only applies in payment with card).
+    // ERROR: internal specific error of the payment method.
+    switch (transaction.status) {
+      case 'APPROVED':
+        this.paymentService.updatePaymentStatus(
+          transaction.reference,
+          'APPROVED',
+          transaction.payment_method_type,
+        );
+        break;
+      case 'DECLINED':
+        this.paymentService.updatePaymentStatus(
+          transaction.reference,
+          'DECLINED',
+          transaction.payment_method_type,
+        );
+        break;
+      case 'VOIDED':
+        this.paymentService.updatePaymentStatus(
+          transaction.reference,
+          'VOIDED',
+          transaction.payment_method_type,
+        );
+        break;
+      case 'ERROR':
+        this.paymentService.updatePaymentStatus(
+          transaction.reference,
+          'ERROR',
+          transaction.payment_method_type,
+        );
+        break;
+    }
   }
 }
--- src/webhook/webhook.module.ts
+++ src/webhook/webhook.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { WebhookService } from './webhook.service';
 import { WebhookController } from './webhook.controller';
+import { PaymentModule } from 'src/payments/payment.module';
 
 @Module({
+  imports: [PaymentModule],
   controllers: [WebhookController],
   providers: [WebhookService],
 })
