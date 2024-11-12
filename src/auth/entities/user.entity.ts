d8b7f22b620ce46b9dcb5e28316b8c4e7632da58
DANIELSSF <daniel.santiago730@gmail.com>
2024-11-12 12:09:33 -0500

    Add one-to-many relationship between User and Payment entities

--- src/auth/entities/user.entity.ts
+++ src/auth/entities/user.entity.ts
@@ -1,4 +1,5 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Payment } from 'src/payments/entities/payment.entity';
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
 
 @Entity('users')
 export class User {
@@ -22,4 +23,7 @@ export class User {
 
   @Column()
   picture: string;
+
+  @OneToMany(() => Payment, (payment) => payment.user)
+  payments: Payment[];
 }
